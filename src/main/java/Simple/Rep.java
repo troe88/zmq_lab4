3d7d47539f04794feafd9cb301266a022e3d60af
dmitry <dmitry.trok@gmai.com>
2015-03-25 22:49:11 +0300

    first finish

--- src/main/java/Simple/Rep.java
+++ src/main/java/Simple/Rep.java
@@ -1,7 +1,30 @@
 package Simple;
 
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
 public class Rep {
-	public static void main(final String[] args) {
-		System.out.println("REP");
+	private static String request = new String();
+
+	public static void main(final String[] args) throws Exception {
+		System.out.println("Server start");
+		
+		Context context = ZMQ.context(1);
+		
+		Socket socket = context.socket(ZMQ.REP);
+		socket.bind("tcp://*:5555");
+
+		while (!request.equals("exit")) {
+			request = new String(socket.recv(0));
+			System.out.println("Received " + request);
+
+			Thread.sleep(1000);
+
+			String reply = Integer.toString(request.hashCode());
+			socket.send(reply.getBytes(), 0);
+		}
+		socket.close();
+		context.term();
 	}
 }
--- src/main/java/Simple/Req.java
+++ src/main/java/Simple/Req.java
@@ -1,7 +1,35 @@
 package Simple;
 
+import java.util.Scanner;
+
+import org.zeromq.ZMQ;
+
 public class Req {
+	private static final String TCP_LOCALHOST_5555 = "tcp://localhost:5555";
+
 	public static void main(final String[] args) {
-		System.out.println("REQ");
+
+		Scanner scanner = new Scanner(System.in);
+		
+		ZMQ.Context context = ZMQ.context(1);
+
+		System.out.println("Connecting to " + TCP_LOCALHOST_5555);
+
+		ZMQ.Socket requester = context.socket(ZMQ.REQ);
+		requester.connect(TCP_LOCALHOST_5555);
+
+		String request = new String();
+		while(!request.equals("exit")){
+			request = scanner.next();
+			System.out.println("Sending " + request);
+			requester.send(request.getBytes(), 0);
+
+			byte[] reply = requester.recv(0);
+			System.out.println("Received " + new String(reply));
+		}
+		
+		scanner.close();
+		requester.close();
+		context.term();
 	}
 }
