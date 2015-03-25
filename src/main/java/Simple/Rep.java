4abd0faf50610269e6d0f332ad9252d0215fbf14
dmitry <dmitry.trok@gmai.com>
2015-03-26 00:01:29 +0300

    q

--- src/main/java/Simple/Rep.java
+++ src/main/java/Simple/Rep.java
@@ -1,7 +1,30 @@
 package Simple;
 
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
 public class Rep {
-	public static void main(final String[] args) {
-		System.out.println("REP");
+	private static final String TCP_2222 = "tcp://*:2222";
+	private static String msg = new String();
+
+	public static void main(final String[] args) throws InterruptedException {
+		System.out.println("Server start!");
+
+		Context context = ZMQ.context(1);
+		Socket socket = context.socket(ZMQ.REP);
+		socket.bind(TCP_2222);
+
+		while (!Thread.currentThread().isInterrupted()) {
+			msg = socket.recvStr();
+			System.out.println(msg);
+
+			Thread.sleep(1000);
+
+			socket.send("Recv: " + msg);
+		}
+
+		socket.close();
+		context.term();
 	}
 }
--- src/main/java/Simple/Req.java
+++ src/main/java/Simple/Req.java
@@ -1,7 +1,35 @@
 package Simple;
 
+import java.util.Scanner;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
 public class Req {
+	private static final String TCP_LOCALHOST_2222 = "tcp://localhost:2222";
+	private static String msg_from_server = new String();
+	private static String msg_to_server = new String();
+
 	public static void main(final String[] args) {
-		System.out.println("REQ");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		Context context = ZMQ.context(1);
+		Socket socket = context.socket(ZMQ.REQ);
+		socket.connect(TCP_LOCALHOST_2222);
+		
+		while (!msg_to_server.equals("exit")) {
+			msg_to_server = scanner.next();
+			socket.send(msg_to_server);
+			
+			msg_from_server  = socket.recvStr();
+			System.out.println(msg_from_server );
+		}
+		
+		scanner.close();
+		socket.close();
+		context.term();
+		
 	}
 }
